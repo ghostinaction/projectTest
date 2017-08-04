d74eb79c8c537c91f5b70c82daf6e99c910bb726
OuYongQiang <361246450@qq.com>
2017-08-05 02:12:15 +0800

    Update HelloWorld.java

--- src/projectTest/HelloWorld.java
+++ src/projectTest/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello git I am comming");
 		String aString = "helloworld";
-		String aString2 = "123";
+		String aString2 = "12312";
 	}
 }
