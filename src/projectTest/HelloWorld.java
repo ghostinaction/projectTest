88388c6d4bece0297a307a5b3241cc1d37d6039d
Ghostinaction <361246450@qq.com>
2017-08-05 02:13:30 +0800

    1111

--- src/projectTest/HelloWorld.java
+++ src/projectTest/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello git I am comming");
 		String aString = "helloworld";
+		String aString2 = "123";
 	}
 }
